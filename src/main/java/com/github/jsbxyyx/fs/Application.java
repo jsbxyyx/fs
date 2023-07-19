6efa1935992b1f558c4a064deefcd1aade91e31b
jsbxyyx <jsbxyyx@163.com>
2023-07-19 21:42:35 +0800

    add tip

--- src/main/java/com/github/jsbxyyx/fs/Application.java
+++ src/main/java/com/github/jsbxyyx/fs/Application.java
@@ -1,15 +1,22 @@
 package com.github.jsbxyyx.fs;
 
 import com.github.jsbxyyx.fs.controller.NetUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication(scanBasePackages = {"com.github.jsbxyyx"})
 public class Application {
 
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-        System.out.println("local ip : " + NetUtil.getLocalIp("192.", "10."));
+        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+        String port = context.getEnvironment().getProperty("server.port");
+        log.info("\n\n\thttp://{}:{}\n\thttp://127.0.0.1:{}",
+                NetUtil.getLocalIp("192.", "10."), port, port);
     }
 
 }
