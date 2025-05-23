f5b9361a4c89f7c5478e022646aff41861eb120f
jsbxyyx <jsbxyyx@163.com>
2025-05-23 11:29:23 +0800

    Update fs.bat

--- src/main/resources/fs.bat
+++ src/main/resources/fs.bat
@@ -4,5 +4,5 @@ IF "%JAVA_HOME%" == "" (
     goto end
 )
 set pwd=%~dp0
-%JAVA_HOME%\bin\java -Dspring.servlet.multipart.max-file-size=10GB -Dspring.servlet.multipart.max-request-size=10GB -jar %pwd%fs.jar %1
+%JAVA_HOME%\bin\java -Dspring.servlet.multipart.max-file-size=10GB -Dspring.servlet.multipart.max-request-size=10GB -jar %pwd%fs.jar %*
 :end
